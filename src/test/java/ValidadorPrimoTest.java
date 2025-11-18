8259ab4d80209b6a979531aba9c6f31c1a7aba29
JoelIbarra1 <105898492+JoelIbarra1@users.noreply.github.com>
2025-11-18 00:38:34 +0000

    2

--- src/test/java/ValidadorPrimoTest.java
+++ src/test/java/ValidadorPrimoTest.java
@@ -12,4 +12,31 @@ public class ValidadorPrimoTest {
         assertTrue(ValidadorPrimo.esPrimo(2));
     }
 
+    @Test
+    public void testTresEsPrimo(){
+        assertTrue(ValidadorPrimo.esPrimo(3));
+    }
+
+    @Test
+    public void testCuatroNoEsPrimo(){
+        assertFalse(ValidadorPrimo.esPrimo(4));
+    }
+
+    @Test
+    public void testNegativoNoEsPrimo(){
+        assertFalse(ValidadorPrimo.esPrimo(-7));
+    }
+
+    @Test
+    public void testPrimoGrande(){
+        assertTrue(ValidadorPrimo.esPrimo(7919));
+    }
+
+    @Test
+    public void testCuadradoNoEsPrimo(){
+        assertFalse(ValidadorPrimo.esPrimo(49));
+    }
+
+
+
 }
